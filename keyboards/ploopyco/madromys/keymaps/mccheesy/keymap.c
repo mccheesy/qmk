0b480886f50cf8e8aaf948eb29804fd30297c563
James D McCleese <jmccleese@gmail.com>
2025-04-24 22:46:14 -0500

    feat: add madromys/mccheesy keymap

---
+++ keyboards/ploopyco/madromys/keymaps/mccheesy/keymap.c
@@ -0,0 +1,31 @@
+#include QMK_KEYBOARD_H
+
+#include "config.h"
+
+const uint16_t PROGMEM dpi_combo[]    = {KC_BTN4, KC_BTN5, COMBO_END};
+const uint16_t PROGMEM layer0_combo[] = {KC_BTN1, KC_BTN2, COMBO_END};
+const uint16_t PROGMEM layer1_combo[] = {KC_BTN2, KC_BTN3, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(dpi_combo, DPI_CONFIG),
+    COMBO(layer0_combo, TO(0)),
+    COMBO(layer1_combo, TO(1)),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+//      |-------------|-------------|-------------|-------------|
+//      |   KC_BTN1   |   KC_BTN3   |   KC_BTN2   |   KC_BTN5   |
+//      |-------------|-------------|-------------|-------------|
+//      | DRAG_SCROLL |             |             |   KC_BTN4   |
+//      |-------------|-------------|-------------|-------------|
+            KC_BTN1,      KC_BTN3,      KC_BTN2,      KC_BTN5,
+          DRAG_SCROLL,                                KC_BTN4 ),
+    [1] = LAYOUT(
+//      |-------------|-------------|-------------|-------------|
+//      |   KC_BTN1   | LS(KC_BTN1) | LS(KC_BTN2) |   KC_BTN2   |
+//      |-------------|-------------|-------------|-------------|
+//      | DRAG_SCROLL |             |             |   KC_BTN5   |
+//      |-------------|-------------|-------------|-------------|
+            KC_BTN1,    LS(KC_BTN1),  LS(KC_BTN2),  KC_BTN2,
+          DRAG_SCROLL,                              KC_BTN5 )
+};
