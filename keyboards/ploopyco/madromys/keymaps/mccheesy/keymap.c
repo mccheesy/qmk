f5a5b14a5488df2f300b651a6b36e4db63b2c954
James D McCleese <jmccleese@gmail.com>
2025-04-24 23:33:03 -0500

    feat: add bootloader combo

--- keyboards/ploopyco/madromys/keymaps/mccheesy/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/mccheesy/keymap.c
@@ -5,11 +5,13 @@
 const uint16_t PROGMEM dpi_combo[] = {KC_BTN4, KC_BTN5, COMBO_END};
 const uint16_t PROGMEM layer0_combo[] = {KC_BTN1, KC_BTN2, COMBO_END};
 const uint16_t PROGMEM layer1_combo[] = {KC_BTN2, KC_BTN3, COMBO_END};
+const uint16_t PROGMEM bootloader_combo[] = {KC_BTN1, KC_BTN2, KC_BTN3, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(dpi_combo, DPI_CONFIG),
     COMBO(layer0_combo, TO(0)),
     COMBO(layer1_combo, TO(1)),
+    COMBO(bootloader_combo, QK_BOOT),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
